919849a1051bd21c73c2d287a235eb0044ebad70
NathanLauga <nathan.lauga@gmail.com>
2019-11-16 16:45:20 +0100

    Clean up

--- source/conf.py
+++ source/conf.py
@@ -10,14 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'football-game-predictor'
+project = 'International Football game predictor'
 copyright = '2019, Nathan Lauga'
 author = 'Nathan Lauga'
 
@@ -27,9 +27,7 @@ author = 'Nathan Lauga'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx.ext.autodoc'
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -51,3 +49,6 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Extension configuration -------------------------------------------------
--- source/index.rst
+++ source/index.rst
@@ -1,20 +1,14 @@
-.. football-game-predictor documentation master file, created by
-   sphinx-quickstart on Sat Nov 16 10:35:42 2019.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-Welcome to football-game-predictor's documentation!
-===================================================
-
+Documentation for the Code
+**************************
 .. toctree::
    :maxdepth: 2
-   :caption: Contents:
 
-
+   app.model
+   app.data
+   app.utils
 
 Indices and tables
 ==================
-
 * :ref:`genindex`
 * :ref:`modindex`
 * :ref:`search`
