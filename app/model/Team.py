a5d07b540043991d46ab8d03f1efece7c40d0b7e
NathanLauga <nathan.lauga@gmail.com>
2019-11-16 12:15:50 +0100

    Add: init sphinx doc

--- app/model/Team.py
+++ app/model/Team.py
@@ -3,6 +3,27 @@ from ..utils import array_sum_to_one
 
 class Team:
     ''' 
+    Class containing team information for a game.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        Full name of the team
+    avg_goals : float
+        Average number of goals 
+    proba_goals : list
+        List of scoring probability, the index correspond 
+        to the the number of goals
+
+    Methods
+    -------
+    compute_proba_goals(max_goals)
+        Given a max number of goals it computes a poisson distribution
+        based on the avg_goals attribute and as a result it gives the
+        score probability for 0 to the maximumn number of goals 
+
     '''
 
     def __init__(self, name: str):
@@ -12,6 +33,14 @@ class Team:
 
     def compute_proba_goals(self, max_goals: int):
         '''
+        Given a max number of goals it computes a poisson distribution
+        based on the avg_goals attribute and as a result it gives the
+        score probability for 0 to the maximumn number of goals 
+
+        Parameters
+        ----------
+        max_goals : int
+            the maximum number of goals 
         '''
         self.proba_goals = [poisson.pmf(i, self.avg_goals) for i in range(0, max_goals+1)]
         self.proba_goals = list(array_sum_to_one(self.proba_goals))
---
+++ source/index.rst
@@ -0,0 +1,20 @@
+.. football-game-predictor documentation master file, created by
+   sphinx-quickstart on Sat Nov 16 10:35:42 2019.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to football-game-predictor's documentation!
+===================================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
