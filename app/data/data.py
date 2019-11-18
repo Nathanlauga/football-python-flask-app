98ef4b5348899a91d20e0475c89ca4aa07a09d48
NathanLauga <nathan.lauga@gmail.com>
2019-11-18 12:11:21 +0100

    Add: scrapping real games, predict for real games

--- app/data/data.py
+++ app/data/data.py
@@ -17,3 +17,22 @@ def load_countries(path: str):
         countries name with their flag svg name
     """
     return pd.read_json(path+'countries.json', orient='index', typ='series')
+
+
+def load_results(path: str):
+    """
+    Load the file 'results.csv' that contains all international games
+    since the beginning of this model
+
+    Parameters
+    ----------
+    path : str
+        path to the directory which contains the csv file
+
+    Returns
+    -------
+    pd.DataFrame
+        all international games since 2019-11-14
+    """
+    return pd.read_csv(path+'results.csv')
+
--- app/views.py
+++ app/views.py
@@ -4,21 +4,40 @@ import json
 
 from .model import model, Game
 from .component import IndexForm
-from .data import countries
+from .data import countries, results
 
 from app import app
+
+
+def predict_score_new_games(results):
+    data = pd.read_csv('app/data/results_pred.csv')
+
+    max_date = max(data['date']) if len(data) > 0 else '13-11-2019'
+    results = results[results['date'] > max_date]
+    pred_df = []
+    if len(results) > 0:
+        for index, row in results.iterrows():
+            pred_row = row
+            game = Game(model, row['home_team'], row['away_team'])
+            pred_row['home_team_score'] = game.result[0][0]
+            pred_row['away_team_score'] = game.result[1][0]
+            pred_df += [pred_row.to_frame().T]
+
+        pred_df = pd.concat(pred_df)
+        pred_df.to_csv('app/data/results_pred.csv', index=False)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = IndexForm()
     form.init_choice(countries.index.values)
+    predict_score_new_games(results)
 
     if request.method == 'POST':
         teams = request.form.to_dict()
         if form.validate():
             game = Game(model, team_1=teams['team'], team_2=teams['opponent'])
-            game.compute_result()
+            # game.compute_result()
             return render_template("index.html", teams=countries, form=form, game=game)
 
     return render_template("index.html", teams=countries, form=form)
@@ -34,10 +53,11 @@ def game(game):
         return render_template("404.html")
 
     game = Game(model, team_1=team, team_2=opponent)
-    game.compute_result()
+    # game.compute_result()
 
     return render_template("game.html", game=game, teams=countries)
 
-@app.errorhandler(404) 
-def not_found(e): 
-  return render_template("404.html") 
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
