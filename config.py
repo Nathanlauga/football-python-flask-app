079d4e6b92ab88616a62b490d154bd8b3c4be1c8
NathanLauga <nathan.lauga@gmail.com>
2019-11-18 23:09:40 +0100

    Fix: gunicorn

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ class Config:
     """Set Flask configuration vars from .env file."""
 
     # General Config
-    SECRET_KEY = environ.get('SECRET_KEY')
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
+    SECRET_KEY = 'this_is_the_s3cret_k3y'
+    FLASK_APP = 'run'
+    FLASK_ENV = 'production'
     DEBUG = False
 
     # Specific Config
