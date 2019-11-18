196cbda4647288898efedbccb8402c5dd5eb056d
NathanLauga <nathan.lauga@gmail.com>
2019-11-18 22:56:23 +0100

    Fix: .env

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = environ.get('SECRET_KEY')
     FLASK_APP = environ.get('FLASK_APP')
     FLASK_ENV = environ.get('FLASK_ENV')
-    DEBUG = True
+    DEBUG = False
 
     # Specific Config
     MODEL_FILE = 'model.plk'
